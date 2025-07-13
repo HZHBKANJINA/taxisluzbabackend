0db74721439d3b9f3de772e8aae741fcc4ace96e
HZHBKANJINA <hzhbkanjina@gmail.com>
2025-07-13 15:16:43 +0200

    dodan kontroler za adresu

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaxiSluzbaBackend.Data;
+using TaxiSluzbaBackend.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace TaxiSluzbaBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AddressController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddresses()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddressById(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+            return address == null ? NotFound() : Ok(address);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Address>> CreateAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(CreateAddress), new { id = address.AddressId }, address);
+        }
+    }
+}
